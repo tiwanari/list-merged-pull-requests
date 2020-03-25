df54cfc38afd992c5ff136aff1344988ef66e861
tiwanari <tatsuyaw0c@gmail.com>
2020-03-25 23:56:00 +0900

    Check if comments are empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 export GITHUB_TOKEN=$0
 
-TARGET=$(/find_target.rb)
+TARGET=origin/$(/find_target.rb)
 
 echo "target branch: $TARGET"
 
@@ -16,10 +16,13 @@ git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git || true
 ls -al
 
 cd $(basename $GITHUB_REPOSITORY)
-merges=$(git log origin/$TARGET.. --merges --pretty=format:'* %s --- %b' | sed -E 's/Merge pull request (.*) from .* --- /\1: /g' | grep -v -- '---')
+merges=$(git log $TARGET.. --merges --pretty=format:'* %s --- %b' \
+  | sed -E 's/Merge pull request (.*) from .* --- /\1: /g' \
+  | grep -v -- '---')
 
-echo "$merges"
-
-/make_comment.rb merges
-
-echo "commented"
+if test -z $merges; then
+  echo "No new merge requests on $TARGET"
+else
+  echo "Commenting..."
+  /make_comment.rb merges
+fi
