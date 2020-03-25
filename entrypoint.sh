f64d0f699bca6a618b552c6a5bb239228a917604
tiwanari <tatsuyaw0c@gmail.com>
2020-03-26 01:03:40 +0900

    Fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -eu
 
-GITHUB_TOKEN=$0
+GITHUB_TOKEN=$1
 
 git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 cd $(basename $GITHUB_REPOSITORY)
@@ -12,12 +12,10 @@ echo "Target branch: $TARGET"
 
 CHANGES=$(git log $TARGET.. --merges --pretty=format:'* %s --- %b' \
    | sed -E 's/Merge pull request (.*) from .* --- /\1: /g')
-
 echo "Changes: "
 echo "$CHANGES"
 
 VALID_COMMENTS=$(echo "$CHANGES" | grep -v -- '---')
-
 echo "Valid comments: "
 echo "$VALID_COMMENTS"
 
