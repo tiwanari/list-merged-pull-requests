351c0a44e3b0a26958e56b857f9be06d21c4a99e
tiwanari <tatsuyaw0c@gmail.com>
2020-03-25 23:00:50 +0900

    Clone repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ set -eu
 
 TARGET=$(/find_target.rb)
 
+echo "target branch: $TARGET"
+
+git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git $REPO_NAME
+
+cd $(basename $GITHUB_REPOSITORY)
 merges=$(git log $TARGET.. --merges --pretty=format:'* %s --- %b' \
   | sed -E 's/Merge pull request (.*) from .* --- /\1: /g' \
   | grep -v -- '---')
--- make_comment.rb
+++ make_comment.rb
@@ -11,4 +11,4 @@ message = ARGV[0]
 event = JSON.parse(File.read(ENV.fetch('GITHUB_EVENT_PATH')))
 
 GITHUB = Octokit::Client.new(access_token: ENV.fetch('GITHUB_TOKEN'))
-GITHUB.add_comment(event['repository']['full_name'], event['number'], message)
+GITHUB.add_comment(ENV.fetch('GITHUB_REPOSITORY'), event['number'], message)
