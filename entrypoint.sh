8c1f55e1e41531c5526ad5ccf21176f1457ec40a
tiwanari <tatsuyaw0c@gmail.com>
2020-03-25 22:37:36 +0900

    Check / directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 set -eu
 
-CURRENT_DIR=$(pwd)
-TARGET=$($CURRENT_DIR/find_target.rb)
+TARGET=$(/find_target.rb)
 
 merges=$(git log $TARGET.. --merges --pretty=format:'* %s --- %b' \
   | sed -E 's/Merge pull request (.*) from .* --- /\1: /g' \
   | grep -v -- '---')
 
-$CURRENT_DIR/make_comment.rb merges
+/make_comment.rb merges
